e19ceb5076d1b0144392ad7b0d8b624206bcf4b2
SunitiJain111 <suniti135btcse17@gmail.com>
2021-06-17 10:12:10 +0530

    modified 2 filesin main

--- Ccode/second.c
+++ Ccode/second.c
@@ -1,1 +1,15 @@
 //yoho
+//
+//
+int main()
+
+
+  { 
+ 
+ int x,y,z;
+ cin>>x>>y>>z;
+
+ cout<<sum( x,y,z;
+		return 0;
+ 
+  }
--- javaCode/Hello.java
+++ javaCode/Hello.java
@@ -1,6 +1,10 @@
+import System.Utills.*
+
 class Hello{
 
 public static void main( String[] a){
 	System.out.println("helo suniti");
+	Scanner ip = new Scanner();
+	int a = ip.newxt.nextInt();
 }
 }
